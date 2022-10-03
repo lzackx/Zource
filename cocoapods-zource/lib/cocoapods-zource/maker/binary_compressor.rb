1394b3a5c5675488c625dc4f1e31b7a0f04b6be3
lZackx <lzackx@lzackx.com>
2022-10-03 21:07:36 +0800

    add: compressor

---
+++ cocoapods-zource/lib/cocoapods-zource/maker/binary_compressor.rb
@@ -0,0 +1,31 @@
+require "cocoapods"
+
+module CocoapodsZource
+  class ZourcePod
+    class BinaryCompressor
+      attr_reader :zource_pod
+
+      def initialize(zource_pod)
+        @zource_pod = zource_pod
+      end
+
+      def compress
+        Dir::chdir(@zource_pod.zource_pod_binary_path)
+        executable = "zip"
+        command = Array.new
+        command << "-r"
+        command << "#{@zource_pod.zip_path}"
+        command << "./*"
+        # raise_on_failure = true
+        full_command = "#{executable} #{command.join(" ")}"
+        begin
+          system("#{full_command}")
+        rescue Exception => e
+          abort("compress exception:\n#{e}")
+        end
+      end
+
+      # End Class
+    end
+  end
+end
